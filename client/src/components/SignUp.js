0cc925076ccaf2dee304824f6e5640edad3a1127
Prince <avecillasprince@yahoo.com>
2021-09-22 16:03:57 -0400

    Done

---
+++ client/src/components/SignUp.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+
+function SignUp({ setUser }) {
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [image_url,setImageUrl] = useState("")
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        first_name,
+        last_name,
+        username,
+        email,
+        image_url,
+        password,
+        password_confirmation: passwordConfirmation,
+      }),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h1>Sign Up</h1>
+        <label htmlFor="username">First Name</label>
+        <input
+          type="text"
+          id="first_name"
+          autoComplete="off"
+          value={first_name}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <label htmlFor="username">Last Name</label>
+        <input
+          type="text"
+          id="last_name"
+          autoComplete="off"
+          value={last_name}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <label htmlFor="username">Email</label>
+        <input
+          type="text"
+          id="email"
+          autoComplete="off"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="username">Profile Picture</label>
+        <input
+          type="text"
+          id="username"
+          autoComplete="off"
+          value={image_url}
+          onChange={(e) => setImageUrl(e.target.value)}
+        />
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          autoComplete="off"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          autoComplete="current-password"
+        />
+        <label htmlFor="password">Password Confirmation</label>
+        <input
+          type="password"
+          id="password_confirmation"
+          value={passwordConfirmation}
+          onChange={(e) => setPasswordConfirmation(e.target.value)}
+          autoComplete="current-password"
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignUp;
